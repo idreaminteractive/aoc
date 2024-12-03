23e60698a899bf6e7ed4fffc230db14ee2508643
Dave Wiper <dwiper@idreaminteractive.com>
2024-12-03 11:54:43 +0000

    scuffed

--- 2024/3/part2/main.go
+++ 2024/3/part2/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"regexp"
 
 	"github.com/davecgh/go-spew/spew"
 )
@@ -20,11 +18,14 @@ func main() {
 		log.Fatal(err)
 	}
 	data := string(f)
-	spew.Dump(data)
-	var re = regexp.MustCompile(`(?m)(mul\(\d{1,3},\d{1,3}\))`)
-	var str = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
-	for i, match := range re.FindAllString(str, -1) {
-		fmt.Println(match, "found at index", i)
+	// create our finite state machine
+	var state rune = 'm'
+
+	for _, r := range data {
+		if r == state {
+			spew.Dump("FOUND", r)
+		}
 	}
+
 }
