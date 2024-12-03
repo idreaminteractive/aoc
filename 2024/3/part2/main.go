6ec76fc66533e1903b9f55b1061e46d8d1aa4871
Dave Wiper <dwiper@idreaminteractive.com>
2024-12-03 13:30:54 +0000

    cursed, but works

--- 2024/3/part2/main.go
+++ 2024/3/part2/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -16,15 +21,31 @@ func main() {
 		log.Fatal(err)
 	}
 	data := string(f)
+	// spew.Dump(data)
+	var re = regexp.MustCompile(`(?m)((mul\((\d{1,3}),(\d{1,3})\))|(don't\(\))|(do\(\)))`)
 
-	on := true
-	i := 0
-	for {
+	matches := re.FindAllStringSubmatch(data, -1)
+	// spew.Dump(matches)
+	val := 0
 
-		if on {
-			// we're looking for mul( x, y )
+	do := true
+	for _, m := range matches {
+		if strings.HasPrefix(m[0], "mul") {
+			if do {
+				m1, _ := strconv.Atoi(m[3])
+				m2, _ := strconv.Atoi(m[4])
+				val += m1 * m2
+			}
 		}
-
+		if strings.HasPrefix(m[0], "don't") {
+			do = false
+		}
+		if strings.HasPrefix(m[0], "do()") {
+			do = true
+		}
 	}
-
+	spew.Dump(val)
+	// for i, match := range re.FindAllString(data, -1) {
+	// 	fmt.Println(match, "found at index", i)
+	// }
 }
